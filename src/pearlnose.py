e8f2a514c250ac1be61861a23bfdb39aee017473
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-06-16 12:14:48 -0400

    getting matplotib to work

--- src/pearlnose.py
+++ src/pearlnose.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import dlib
 from math import hypot
+import matplotlib.pyplot as plt
 
 cap = cv2.VideoCapture(0)
 
@@ -14,6 +15,7 @@ cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 nose_image = cv2.imread("1.png")
+eye_image = cv2.imread("eyeL.png")
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (44,426)
@@ -32,7 +34,9 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rows, cols, _ = frame.shape
     nose_mask = np.zeros((rows, cols), np.uint8)
+    eyeLmask = np.zeros((rows,cols), np.uint8)
     nose_mask.fill(0)
+    eyeLmask.fill(0)
     
     faces = detector(gray)
     for face in faces:
@@ -40,7 +44,7 @@ while True:
         face_top = face.top()
         face_right = face.right()
         face_bottom = face.bottom()
-        cv2.rectangle(gray, (face_left, face_top), (face_right, face_bottom), (0, 255, 0), 3)
+        cv2.rectangle(frame, (face_left, face_top), (face_right, face_bottom), (0, 255, 0), 3)
         
         
 
@@ -57,34 +61,32 @@ while True:
             nose_left = (landmarks.part(31).y, landmarks.part(31).x)
             nose_right = (landmarks.part(35).y, landmarks.part(35).x)
 
-            right_eyebrow_1= (landmarks.part(22).y, landmarks.part(22).x)
-            right_eyebrow_2= (landmarks.part(23).y, landmarks.part(23).x)
-            right_eyebrow_3 =(landmarks.part(24).y, landmarks.part(24).x)
+            
             right_eye_left_point = (landmarks.part(42).x, landmarks.part(42).y)
             right_eye_right_point = (landmarks.part(45).x, landmarks.part(45).y)
             
             left_eye_left_point = (landmarks.part(36).x, landmarks.part(36).y)
             left_eye_right_point = (landmarks.part(39).x, landmarks.part(39).y)
             
-            left_eye_center_top = midpoint(landmarks.part(37), landmarks.part(38))
-            left_eye_center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+            left_eye_center_top = midpoint(landmarks.part(37), landmarks.part(37))
+            left_eye_center_bottom = midpoint(landmarks.part(40), landmarks.part(40))
             
             right_eye_center_top= midpoint(landmarks.part(43), landmarks.part(44))
             right_eye_center_bottom = midpoint(landmarks.part(46), landmarks.part(47))
 
-            left_eye_hor_line = cv2.line(gray, left_eye_left_point, left_eye_right_point, (0, 255, 0), 2)
-            left_eye_ver_line = cv2.line(gray, left_eye_center_top, left_eye_center_bottom, (0, 255, 0), 2)
+            left_eye_hor_line = cv2.line(frame, left_eye_left_point, left_eye_right_point, (0, 255, 0), 2)
+            left_eye_ver_line = cv2.line(frame, left_eye_center_top, left_eye_center_bottom, (0, 255, 0), 2)
             
-            right_eye_hor_line = cv2.line(gray, right_eye_left_point, right_eye_right_point, (0, 255, 0), 2)
-            right_eye_ver_line = cv2.line(gray, right_eye_center_top, right_eye_center_bottom, (0, 255, 0), 2)
+            right_eye_hor_line = cv2.line(frame, right_eye_left_point, right_eye_right_point, (0, 255, 0), 2)
+            right_eye_ver_line = cv2.line(frame, right_eye_center_top, right_eye_center_bottom, (0, 255, 0), 2)
             
            
-            eye_width = (hypot(left_eye_left_point[0] - left_eye_right_point[0], left_eye_left_point[1] - left_eye_right_point[1]))
+            eyeL_width = (hypot(left_eye_left_point[0] - left_eye_right_point[0], left_eye_left_point[1] - left_eye_right_point[1]))
             
             nose_width = (hypot(nose_left[0] - nose_right[0], nose_left[1]- nose_right[1] ))
             nose_height = (nose_width*0.77)
             
-            eye_height = (eye_width*0.77)
+            eyeL_height = (eyeL_width*0.77)
             
             
                     # New nose position
@@ -93,8 +95,14 @@ while True:
             bottom_right = (int(nose_center[0] + nose_width / 2),
                        int(nose_center[1] + nose_height / 2))
             
+            top_left_eye = (int(left_eye_left_point[0] -eyeL_width / 2),
+                              int(left_eye_left_point[1] - eyeL_height / 2))
+            
+            bottom_right = (int(left_eye_right_point[0] + eyeL_width / 2),
+                int(left_eye_right_point[1] + eyeL_height / 2))
+            
             dim =(int(nose_width),int(nose_height))
-            dimeye =(int(eye_width),int(eye_height))
+            eyeLdim =(int(eyeL_width),int(eyeL_height))
             
             cv2.putText(frame,'nose pos new'+str(dim), 
                 bottomLeftCornerOfText, 
@@ -103,7 +111,7 @@ while True:
                 fontColor,
                 lineType)
             
-            cv2.putText(frame,'eye pos new'+str(dimeye), 
+            cv2.putText(frame,'eyeLEFT pos new'+str(eyeLdim), 
                 bottomLeftCornerOfText2, 
                 font, 
                 fontScale,
@@ -124,11 +132,23 @@ while True:
             frame[int(top_left[1]): int(top_left[1]) + int(nose_height),
             int(top_left[0]): int(top_left[0]) + int(nose_width)] = final_nose
 
-            cv2.imshow("Nose area", nose_area)
-            cv2.imshow("Nose pig", nose_pig)
+  
+            eyeL_pig = cv2.resize(eye_image, eyeLdim)
+            #eyeL_gray = cv2.cvtColor(eyeL_pig, cv2.COLOR_BGR2GRAY)
+            #_,  eyeLmask = cv2.threshold(eyeLmask, 25, 255, cv2.THRESH_BINARY_INV)   
+            # eyeL_area_no_nose = cv2.bitwise_and(eyeL_area, eyeL_area, mask=eyeLmask)
+           #final_eye = cv2.add(eyeL_area_no_nose, eyeL_pig)
+            #frame[int(top_left[1]): int(top_left[1]) + int(eyeL_height),
+            #int(top_left_eye[0]): int(top_left_eye[0]) + int(eyeL_width)] = final_eye
+            plt.title("face Graphic")
+            plt.plot(dim,eyeLdim)    
+            plt.show()        
+            #cv2.imshow("Nose area", frame)
+            #cv2.imshow("Nose pig", eye_image)
             cv2.imshow("final nose", final_nose)
+            #cv2.imshow("finalEyee",gray)
             
-            cv2.circle(gray, (x, y), 4, (255, 0, 0), -1)
+            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
 
 
     cv2.imshow("Frame", frame)
